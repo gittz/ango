6d5594fe0cba39c771f8c4a80ac5bf32926b93b8
guy <guy@finereport.com>
2017-11-09 17:09:24 +0800

    a

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ import {
 import {defineReactive, observerState} from "./vue/observer/index";
 import Watcher from "./vue/observer/watcher";
 import {extend} from "./preact/util";
-import {noop} from "./vue/shared/util";
+import {isPlainObject, noop} from "./vue/shared/util";
+import Dep from "./vue/observer/dep";
+import {nativeWatch} from "./vue/util/env";
 
 //配置
 options.beforeUnmount = function (component) {
@@ -21,6 +23,94 @@ options.beforeUnmount = function (component) {
     while (i--) {
         component._watchers[i].teardown()
     }
+}
+
+const computedWatcherOptions = {lazy: true}
+
+function initComputed(vm, computed) {
+    const watchers = vm._computedWatchers = Object.create(null)
+    // computed properties are just getters during SSR
+
+    for (const key in computed) {
+        const userDef = computed[key]
+        const getter = typeof userDef === 'function' ? userDef : userDef.get
+        // create internal watcher for the computed property.
+        watchers[key] = new Watcher(
+            vm,
+            getter || noop,
+            noop,
+            computedWatcherOptions
+        )
+
+        // component-defined computed properties are already defined on the
+        // component prototype. We only need to define computed properties defined
+        // at instantiation here.
+        if (!(key in vm)) {
+            defineComputed(vm, key, userDef)
+        }
+    }
+}
+
+export function defineComputed(target, key, userDef) {
+    const shouldCache = true
+    if (typeof userDef === 'function') {
+        sharedPropertyDefinition.get = shouldCache
+            ? createComputedGetter(key)
+            : userDef
+        sharedPropertyDefinition.set = noop
+    } else {
+        sharedPropertyDefinition.get = userDef.get
+            ? shouldCache && userDef.cache !== false
+                ? createComputedGetter(key)
+                : userDef.get
+            : noop
+        sharedPropertyDefinition.set = userDef.set
+            ? userDef.set
+            : noop
+    }
+    Object.defineProperty(target, key, sharedPropertyDefinition)
+}
+
+function createComputedGetter(key) {
+    return function computedGetter() {
+        const watcher = this._computedWatchers && this._computedWatchers[key]
+        if (watcher) {
+            if (watcher.dirty) {
+                watcher.evaluate()
+            }
+            if (Dep.target) {
+                watcher.depend()
+            }
+            return watcher.value
+        }
+    }
+}
+
+function initWatch(vm, watch) {
+    for (const key in watch) {
+        const handler = watch[key]
+        if (Array.isArray(handler)) {
+            for (let i = 0; i < handler.length; i++) {
+                createWatcher(vm, key, handler[i])
+            }
+        } else {
+            createWatcher(vm, key, handler)
+        }
+    }
+}
+
+function createWatcher(vm,
+                       keyOrFn,
+                       handler,
+                       options) {
+    if (isPlainObject(handler)) {
+        options = handler
+        handler = handler.handler
+    }
+    if (typeof handler === 'string') {
+        handler = vm[handler]
+    }
+    return vm.$watch(keyOrFn, handler, options)
 }
 
 const sharedPropertyDefinition = {
@@ -53,9 +143,29 @@ export class Component extends C {
         for (const key in state) {
             defineReactive(this, key, state[key])
         }
+        if (this.computed) initComputed(this, this.computed)
+        if (this.watch && this.watch !== nativeWatch) {
+            initWatch(this, this.watch)
+        }
         this._watcher = new Watcher(this, () => {
             this.forceUpdate()
         })
+    }
+
+    $watch(expOrFn, cb, options) {
+        const vm = this
+        if (isPlainObject(cb)) {
+            return createWatcher(vm, expOrFn, cb, options)
+        }
+        options = options || {}
+        options.user = true
+        const watcher = new Watcher(vm, expOrFn, cb, options)
+        if (options.immediate) {
+            cb.call(vm, watcher.value)
+        }
+        return function unwatchFn() {
+            watcher.teardown()
+        }
     }
 
     state() {
